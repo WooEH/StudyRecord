5eda50e630d0b05f9dd1d69e58ac0240ddef3d27
WooEH <soubyt5292@gmail.com>
2020-06-18 02:11:04 +0900

    Initial Commit

---
+++ CodingC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CodingC
+//
+//  Created by 우은혁 on 2020/06/18.
+//  Copyright © 2020 우은혁. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
