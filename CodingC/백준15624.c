03471d883298fd6033cb02b300a3d027d9b41bc4
WooEH <soubyt5292@gmail.com>
2020-08-05 20:31:53 +0900

    ㄴㅇㄹㄴㅇㄹㅇㄴㅇㄴ

--- CodingC/백준15624.c
+++ CodingC/백준15624.c
@@ -1,28 +1,29 @@
-#include <stdio.h>
-
-long long Fibo(int n);
-
-int main() {
-    int n;
-    long long ans=0;
-    
-    scanf("%d",&n);
-    
-    if(!(n<=1000000&&n>=0)){
-        return -1;
-    }
-    ans = Fibo(n)%1000000007;
-    printf("%lld",ans);
-    return 0;
-}
-
-long long Fibo(int n) {
-
-    if(n==0) {
-        return 0;
-    }else if(n==1){
-        return 1;
-    }
-    
-    return Fibo(n-1)+Fibo(n-2);
-}
+//#include <stdio.h>
+//
+//long long Fibo(int n);
+//
+//int main() {
+//    int n;
+//    long long ans=0;
+//    
+//    scanf("%d",&n);
+//    
+//    if(!(n<=1000000&&n>=0)){
+//        return -1;
+//    }
+//    ans = Fibo(n)%1000000007;
+//    printf("%lld",ans);
+//    return 0;
+//}
+//
+//long long Fibo(int n) {
+//
+//    if(n==0) {
+//        return 0;
+//    }else if(n==1){
+//        return 1;
+//    }
+//    
+//    return Fibo(n-1)+Fibo(n-2);
+//}
+// 
---
+++ CodingC/백준3190.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int N, K, L, X;
+    int pos_x, pos_y;
+    char C;
+    
+    scanf("%d",&N);
+    if(N<2||N>100){ //예외처리
+        return -1;
+    }
+    
+    scanf("%d",&K); //에외처리
+    if(K<0||K>100){
+        return -1;
+    }
+    
+    for(int i=0;i<K;i++) {
+        scanf("%d %d",&pos_x,&pos_y);
+        if(pos_x==1&&pos_y==1) {    //맨위 맨좌측에 사과 있으면 안됨.
+            return -1;
+        }
+    }
+    
+    scanf("%d",&L);
+    
+    for(int i=0;i<L;i++){
+        scanf("%d %c",&X,&C);
+    }
+    return 0;
+}
