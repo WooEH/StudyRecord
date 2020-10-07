70dcfb5bff07404732e262f212c1852cdf87e6d8
WooEH <soubyt5292@gmail.com>
2020-10-07 17:20:43 +0900

    hhh

---
+++ CodingC/Kinglog16.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//#define PI 3.14
+//
+//int main(){
+//    const int A = 1;
+//    // PI += 1;
+//    printf("%.2lf\n",PI);
+//
+//    //A += 1;
+//    printf("%d",A);
+//    return 0;
+//}
+
---
+++ CodingC/Kinglog17.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//    int *p = NULL;
+//    
+//    p = (int *)malloc(sizeof(int)*5);
+//    if(p==NULL){
+//        return -1;
+//    }
+//    
+//    for(int i=0;i<5;i++){
+//        scanf("%d",p+i);
+//    }
+//    
+//    for(int i=0;i<5;i++){
+//        printf("%d ",*(p+i));
+//    }
+//    
+//    free(p);
+//    return 0;
+//}
